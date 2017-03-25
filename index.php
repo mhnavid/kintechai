97676140467bad0bf48d4e2e81202eff967794b5
Shahidul Islam <shawon_math07@yahoo.com>
2017-03-25 19:37:16 +0600

    5

--- index.php
+++ index.php
@@ -9,12 +9,12 @@
 <div class="body-container">
 	<!-- name -->
 
-	<div class="name-container">
+	<div class="name-container" align="center">
 		<h1 class="name">KINTE CHAI</h1>
 	</div>
 
 	<!-- search-bar -->
-	<div class="search-bar">
+	<div class="search-bar" align="center">
 		<div class="search">
 	      <input type="text" class="searchTerm" placeholder="What are you looking for?">
 	      <button type="submit" class="searchButton">Search</button>
--- login.php
+++ login.php
@@ -12,14 +12,15 @@
 <body>
 <table align="center">
     <tr>
-        <td>Email or Username :</td>
-        <td><input type="text"></td>
+        <td>Email or Username</td>
+        <td>: <input type="text"></td>
     </tr>
     <tr>
         <td>Password</td>
-        <td><input type="password"></td>
+        <td>: <input type="password"></td>
     </tr>
     <tr align="center">
+        <td></td>
         <td colspan="2"><input type="submit" value="Submit"></td>
     </tr>
 </table>
