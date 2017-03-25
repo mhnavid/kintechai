eaa2c4926ac6e1d1bc213bf7f44964ed67b8e03f
DESKTOP-MTL4IDG\hasan <hasan.nvd@gmail.com>
2017-03-25 17:49:41 +0600

    1

--- index.php
+++ index.php
@@ -6,5 +6,22 @@
 </head>
 <body>
 
+<div class="body-container">
+	<!-- name -->
+
+	<div class="name-container">
+		<h1 class="name">KINTE CHAI</h1>
+	</div>
+
+	<!-- search-bar -->
+	<div class="search-bar">
+		<div class="search">
+	      <input type="text" class="searchTerm" placeholder="What are you looking for?">
+	      <button type="submit" class="searchButton">Search</button>
+   		</div>
+	</div>
+
+</div>
+
 </body>
 </html>
