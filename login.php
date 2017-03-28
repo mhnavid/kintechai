d836ada9e71f020f73dc306aa8bf5a9ab581bfef
DESKTOP-MTL4IDG\hasan <hasan.nvd@gmail.com>
2017-03-28 17:18:41 +0600

    14

--- login.php
+++ login.php
@@ -7,23 +7,42 @@
  */
 ?>
 <head>
-	<title>login</title>
+	<title>Login</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
+
+<!-- header -->
+<div class="header" id="header">
+    <div class="header-site">
+        <a href="#" class="header-site-name">
+            <h1>KINTE CHAI</h1>
+        </a>
+    </div>
+</div>
+
 <form action="#" method="post">
-    <table align="center">
-        <tr>
-            <td>Email or Username</td>
-            <td>: <input type="text"></td>
+    <table class="login-input-table">
+        <tr class="login-email-or-username">
+            <td class="login-email-or-username-text">Email or Username</td>
+            <td class="login-email-or-username-input">: <input type="text"></td>
         </tr>
         <tr>
-            <td>Password</td>
-            <td>: <input type="password"></td>
+            <td class="login-password-text">Password</td>
+            <td class="login-password-input">: <input type="password"></td>
         </tr>
-        <tr align="center">
-            <td></td>
+        <tr class="login-submit-button">
             <td colspan="2"><input type="submit" value="Submit"></td>
         </tr>
     </table>
 </form>
+
+<!-- footer -->
+<div class="footer" id="footer">
+    <div class="footer-content">
+        <p>Copyright © 2017. All Rights Reserved by <b>Hexaparity</b>.</p>
+    </div>
+</div>
+
+
 </body>
